f158211acb133f92d01e21c2d893d22129b6b019
Michel <ayres.michel@gmail.com>
2019-04-05 22:13:44 -0300

    Fixing the default from routeTemplate

--- swagger-aspnet-docker/App_Start/WebApiConfig.cs
+++ swagger-aspnet-docker/App_Start/WebApiConfig.cs
@@ -16,7 +16,7 @@ namespace swagger_aspnet_docker
 
             config.Routes.MapHttpRoute(
                 name: "DefaultApi",
-                routeTemplate: "api/{controller}/{id}",
+                routeTemplate: "{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
         }
