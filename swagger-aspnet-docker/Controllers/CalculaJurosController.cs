eb8d5f783a6a6c598c6d361fb796feea20ea03f6
Michel <ayres.michel@gmail.com>
2019-04-05 22:14:41 -0300

    Controller with dual endpoints

---
+++ swagger-aspnet-docker/Controllers/CalculaJurosController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace swagger_aspnet_docker.Controllers
+{
+    public class CalculaJurosController : ApiController
+    {
+        [Route("calculajuros")]
+        [Route("CalculaJuros/ValorFinal")]
+        public string Get(decimal valorinicial, int meses)
+        {
+            var juros = 0.01;
+            var pow = decimal.Parse((Math.Pow((1 + juros), meses)).ToString());
+            var valorFinal = valorinicial * pow;
+            return String.Format("{0:0,00}", valorFinal.ToString("N", new CultureInfo("pt-BR")));
+        }
+    }
+}
