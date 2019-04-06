76f52da1f4999b10c6855ce69313feb7c1158fa5
Michel <ayres.michel@gmail.com>
2019-04-05 22:20:11 -0300

    Finishing up unity test

---
+++ swagger-aspnet-docker.Tests/Controllers/CalculaJurosControllerTest.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Web.Http;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using swagger_aspnet_docker;
+using swagger_aspnet_docker.Controllers;
+
+namespace swagger_aspnet_docker.Tests.Controllers
+{
+    [TestClass]
+    public class CalculaJurosControllerTest
+    {
+        [TestMethod]
+        public void Get()
+        {
+            // Organizar
+            CalculaJurosController controller = new CalculaJurosController();
+
+            // Agir
+            string result = controller.Get(100, 5);
+
+            // Declarar
+            Assert.AreEqual("105,10", result);
+        }
+    }
+}
